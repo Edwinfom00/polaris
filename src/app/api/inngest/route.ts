9b20c0eaae7fc6876b97989043d5f5071ba56bf6
Edwinfom00 <edwinfom05@gmail.com>
2026-03-15 05:48:51 +0100

    second review

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,11 +2,10 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { helloWorld } from "@/inngest/functions";
 
-// Create an API that serves zero functions
+// Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        /* your functions will be passed here later! */
         helloWorld
     ],
 });
